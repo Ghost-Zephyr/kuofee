f515ad44ea7f41fb0b567c3fbfe1e206bd1b72fe
Sivert V. Sæther <sivert.s13@gmail.com>
2019-10-23 08:37:53 +0200

    Reworked templates

---
+++ game/admin.py
@@ -0,0 +1,11 @@
+from flask import abort
+from .jinja import *
+from .jwt import get
+
+def index(db):
+    try:
+        jwt = get()
+        if jwt['admin']:
+            return render_game_template('admin/index.jinja2', "Admin Stuff", db)
+    except:
+        abort(404)
