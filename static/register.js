095bb890584b19de96275cee3fcb3abf51cb7145
Sivert V. Sæther <sivert.s13@gmail.com>
2019-10-25 12:29:27 +0200

    Fixed password match bug in the client js

--- static/register.js
+++ static/register.js
@@ -1,6 +1,6 @@
 $("#registerForm").submit(function(event){
     event.preventDefault();
-    if ($("#pwd").serialize().slice(4) != $("#pwd1").serialize().slice(4)) {
+    if ($("#pwd").serialize().slice(4) != $("#pwd1").serialize().slice(5)) {
         $('#title').html("Passwords does not match!");
         $('#title').attr('style', "color: #C42069;");
     }
@@ -11,7 +11,6 @@ $("#registerForm").submit(function(event){
             data: $(this).serialize(),
             statusCode: {
                 200: function() { window.location.replace("/"); },
-                406: function() { $('#title').html("Passwords does not match!"); $('#title').attr('style', "color: #C42069;"); },
                 409: function() { $('#title').html("Nick taken."); $('#title').attr('style', "color: #C42069;"); },
                 400: function() { $('#title').html("Could not create user."); $('#title').attr('style', "color: #C42069;"); }
             }
